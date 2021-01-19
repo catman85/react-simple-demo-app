760359910534939fc5a199356f3de267b43c9a71
jimmy <jim@thinkpad.gr>
2021-01-19 23:35:21 +0200

    fetching data and setting state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as api_config from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      products: [],
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData() {
+    fetch(api_config.url + "/categories", api_config.settings)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState(() => ({categories: data}))
+      });
+
+    fetch(api_config.url + "/products", api_config.settings)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          products: data,
+        });
+        console.log(this.state.products)
+      });
+  }
+
+  componentWillMount() {
+    this.fetchData()
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
