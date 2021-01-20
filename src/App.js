9d1fcfb8b5a01e2d630f6b3e83d0061659f6de21
jimmy <jim@thinkpad.gr>
2021-01-20 14:33:21 +0200

    order implemented

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
       });
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.fetchData()
   }
 
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -3,18 +3,69 @@ import React from 'react';
 class MainMenu extends React.Component {
     constructor(props){
         super();
+        this.tags= {}
+        this.state={
+            order:""
+        }
+        this.extractTags= this.extractTags.bind(this);
+        this.handleOrderChange=this.handleOrderChange.bind(this);
     }
-    componentDidUpdate(prevProps) {
-    
-    } 
+
+    componentDidUpdate(){
+        console.debug("updated");
+    }
+
+    handleOrderChange(event){
+        this.setState({order: event.target.value});
+    }
+
+    extractTags(){
+        let temp = [] 
+        this.props.products.forEach(element => {
+            temp = [...temp,...element.tags]
+        });
+
+        //unique elements only
+        temp = [...new Set(temp)];
+
+        for(const key of temp){
+            this.tags[key]=true;
+        }
+
+        return this.tags
+    }
+
     render(){
         return (
             <div>
+                <select value={this.state.order} onChange={this.handleOrderChange}>
+                    <option value="asc">ASC</option>
+                    <option value="desc">DESC</option>
+                </select>
+                {Object.keys(this.extractTags()).map((el)=>
+                        <div>
+                            <input type="checkbox" id={el} value={this.tags[el]}/>
+                            <label for={el}>{el}</label><br/>
+                        </div>
+                )}
+
+                <br/>
+
                 {this.props.products
+                .sort((a,b)=>{
+                    if(this.state.order==="asc" || this.state.order===""){
+                        return a.price - b.price
+                    }else{
+                        return b.price - a.price
+                    }
+                })
                 .filter(p => (p.categoryId === this.props.category || this.props.category === ""))
                 .map((el)=>
-                    <li key={el.id}>
+                    <li key={el.id} style={{marginBottom: "1em"}}>
                         {el.name}
+                        <b>{" "+el.price}</b><br/>
+                        {el.description}<br/>
+                        <img src={el.image}/>
                     </li>
                 )}
             </div>
